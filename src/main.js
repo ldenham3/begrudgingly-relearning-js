f9e8f62c0ef500c554c8bdc3a4979f7b7d6b1a2f
Leah Baker <leah@rmotr.com>
2019-04-25 15:31:19 -0700

    this is too small to commit but I cant handle having a commit under my name where theres js in a script tag, cant do it

---
+++ src/main.js
@@ -0,0 +1,2 @@
+// Im not explaining why and how, tutorial. bite me
+console.log('Seriously everything. I hate it. Also its physically painful to leave this in the html file. Edit: finally moved it.');
